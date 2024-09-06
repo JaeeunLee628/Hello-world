5ead58d2cb91ef9907c0a42e41f926f7f68cf2a1
user <user@DESKTOP-DU01E7K>
2024-09-06 11:31:48 +0900

    2024.09.06

---
+++ src/Hello.java
@@ -0,0 +1,16 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//System.out.println("My name is jaeeun");
+		//System.out.print("나는 재은이야 ");
+		//System.out.println("	Reognize difference between println and print?");
+				
+				
+		System.out.println("Name: Jaeeun Lee");
+		System.out.println("Student num.: 22101468 ");
+	}
+
+}
